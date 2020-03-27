6865c73c5372a291b309e3c0983b6df9f03fa876
mao2009 <entrprise@gmail.com>
2020-03-27 19:04:45 +0900

    del brace

--- i-like-and-retweet-thomas.js
+++ i-like-and-retweet-thomas.js
@@ -1,40 +1,39 @@
-{
-    const SCROOL_Y = 1000;
-    const INTERVAL = 3000;
-    const MAX_COUNT = 50;
-    let count = 1;
 
-    const ID = setInterval( () => {
-        const articles =document.querySelectorAll("article");
-        articles.forEach( article => {
+const SCROOL_Y = 1000;
+const INTERVAL = 3000;
+const MAX_COUNT = 50;
+let count = 1;
 
-            //プロモーションにはいいねしない
-            if (article.outerHTML.match(/>プロモーション<\/span>/)){
-                return;
-            }
+const ID = setInterval( () => {
+    const articles =document.querySelectorAll("article");
+    articles.forEach( article => {
 
-            //いいね済はdiv[data-testid="unlike"]
-            const like = article.querySelector('div[data-testid="like"]');
+        //プロモーションにはいいねしない
+        if (article.outerHTML.match(/>プロモーション<\/span>/)){
+            return;
+        }
 
-            if(like){
-                like.click();
-                count++;
-            }
+        //いいね済はdiv[data-testid="unlike"]
+        const like = article.querySelector('div[data-testid="like"]');
+
+        if(like){
+            like.click();
+            count++;
+        }
+        
+        const retweet = article.querySelector('div[data-testid="retweet"]');
+        if(retweet){
+            retweet.click();
             
-            const retweet = article.querySelector('div[data-testid="retweet"]');
-            if(retweet){
-                retweet.click();
-                
-                const confirm = document.querySelector('div[data-testid="retweetConfirm"]');
-                confirm.click();
-            }
-            
-            if (count > MAX_COUNT){
-                clearInterval(ID);
-                return;
-            }
-        });
-        //いいねの対象は表示されている範囲のみなのでスクロールしていく
-        scrollBy(0,SCROOL_Y);
-    },INTERVAL);
-}
+            const confirm = document.querySelector('div[data-testid="retweetConfirm"]');
+            confirm.click();
+        }
+        
+        if (count > MAX_COUNT){
+            clearInterval(ID);
+            return;
+        }
+    });
+    //いいねの対象は表示されている範囲のみなのでスクロールしていく
+    scrollBy(0,SCROOL_Y);
+},INTERVAL);
--- retweet.js
+++ retweet.js
@@ -1,38 +1,38 @@
-{
-    const SCROOL_Y = 1000;
-    const INTERVAL = 3000;
-    const MAX_COUNT = 50;
-    let count = 1;
 
-    const ID = setInterval(
-        () => {
-            const articles =document.querySelectorAll("article");
-            articles.forEach(
-                
-                article => {
+const SCROOL_Y = 1000;
+const INTERVAL = 3000;
+const MAX_COUNT = 50;
+let count = 1;
 
-                    //プロモーションにはいいねしない
-                    if (article.outerHTML.match(/>プロモーション<\/span>/)){
-                        return;
-                    }
-                    const rt = article.querySelector('div[data-testid="retweet"]');
+const ID = setInterval(
+    () => {
+        const articles =document.querySelectorAll("article");
+        articles.forEach(
+            
+            article => {
 
-                    if(rt){
-                        rt.click();
-                        const confirm = document.querySelector('div[data-testid="retweetConfirm"]');
-                        confirm.click();
-                        count++;
-                    }
-
-                    if (count > MAX_COUNT){
-                        clearInterval(ID);
-                        return;
-                    }
+                //プロモーションにはいいねしない
+                if (article.outerHTML.match(/>プロモーション<\/span>/)){
+                    return;
                 }
-            );
+                const rt = article.querySelector('div[data-testid="retweet"]');
 
-            //いいねの対象は表示されている範囲のみなのでスクロールしていく
-            scrollBy(0,SCROOL_Y);
-        }
-    ,INTERVAL);
-}
+                if(rt){
+                    rt.click();
+                    const confirm = document.querySelector('div[data-testid="retweetConfirm"]');
+                    confirm.click();
+                    count++;
+                }
+
+                if (count > MAX_COUNT){
+                    clearInterval(ID);
+                    return;
+                }
+            }
+        );
+
+        //いいねの対象は表示されている範囲のみなのでスクロールしていく
+        scrollBy(0,SCROOL_Y);
+    }
+,INTERVAL);
+
